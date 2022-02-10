56126a7e4af01a245148e9150ac026dadd63593b
Mich Ochieng <michochieng@gmail.com>
2022-02-10 13:41:18 -0800

    Works with numbers up to 999, need to fix 'hundred' not showing up

--- intToString.hs
+++ intToString.hs
@@ -1,14 +1,15 @@
-module Numbers
-where
 
-numberToWord :: Integer -> String
+numberToWord :: Int -> String
 numberToWord n
-  | n < 20      = shortNumber20 n
-  | n < 100     = tensName (n `div` 10) `hyphen`
-                   numberToWord (n `mod` 10)
+  |(n > 9) && (n < 20)    = teens !! (n - 10)
+  | n < 20                = shortNumber20 n
+  | n < 100               = tensName !! ((n `div` 10)-1) `hyphen` numberToWord (n `mod` 10)
+  |(n > 99) && (n < 1000) = shortNumber20 (n `div` 100) `hyphen`  numberToWord (n `mod` 100)
   | otherwise   = "frog"
 
-tensName _ = "thirtyIsh"
+tensName = ["","twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninety"]
+
+teens    = ["ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"]
 
 hyphen :: String -> String -> String
 hyphen "" y =  y
@@ -16,7 +17,7 @@ hyphen x "" = x
 hyphen x y = x ++ "-" ++ y
 
 
-shortNumber20 :: Integer -> String
+shortNumber20 :: Int -> String
 shortNumber20 n =
-  head . drop (fromInteger n) $  theWords where
-    theWords = [ "", "one", "two", "three","four","five", "six", "seven", "eight","nine" ]
+  head . drop (n) $ theWords where
+    theWords = [ "", "one", "two", "three","four","five", "six", "seven", "eight","nine"]
