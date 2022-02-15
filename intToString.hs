541bc1dc398739e0ca82c9c419a57f655e4737e8
Mich Ochieng <michochieng@gmail.com>
2022-02-14 22:25:37 -0800

    Changed 'otherwise' result

--- intToString.hs
+++ intToString.hs
@@ -5,7 +5,7 @@ numberToWord n
   | n < 20                = shortNumber20 n
   | n < 100               = tensName !! ((n `div` 10)-1) `hyphen` numberToWord (n `mod` 10)
   |(n > 99) && (n < 1000) = hundredHyphen (shortNumber20 (n `div` 100)) `hyphen`  numberToWord (n `mod` 100)
-  | otherwise   = "frog"
+  | otherwise   = "That's a lot of digits!"
 
 tensName = ["","twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninety"]
 
